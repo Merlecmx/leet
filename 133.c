f5836db15e6b87257ff0c2c2ebf997f2d9bf722b
gh843260227 <122433441@qq.com>
2018-07-29 11:57:58 +0800

    add a file named 133.c

---
+++ 133.c
@@ -0,0 +1,40 @@
+/**
+ * #define NEIGHBORS_MAX_SIZE 100
+ * struct UndirectedGraphNode {
+ *     int label;
+ *     struct UndirectedGraphNode *neighbors[NEIGHBORS_MAX_SIZE];
+ *     int neighborsCount;
+ * };
+ */
+#define POS  10000
+
+struct UndirectedGraphNode * dfs(struct UndirectedGraphNode *graph, int *has, struct UndirectedGraphNode ** save)
+{
+    
+    if (has[graph->label + POS]) return save[graph->label + POS];
+    struct UndirectedGraphNode *nd = malloc(sizeof(struct UndirectedGraphNode));
+    has[graph->label + POS] = 1;
+    save[graph->label + POS] = nd;
+
+    nd->label = graph->label;
+    nd->neighborsCount = graph->neighborsCount;
+    for(int i= 0; i < graph->neighborsCount; i++){
+
+        if (graph->neighbors[i] == graph) {
+            nd->neighbors[i] = nd;
+        }
+        else {
+             nd->neighbors[i] = dfs(graph->neighbors[i], has, save);
+        }
+    }
+    return nd;
+}
+
+struct UndirectedGraphNode *cloneGraph(struct UndirectedGraphNode *graph) {
+    if (!graph) return NULL;
+    int *has  = (int *) malloc(4* 20000);
+    memset(has, 0, 4*20000);
+    struct UndirectedGraphNode ** save = (struct UndirectedGraphNode **) malloc(20000*sizeof(struct UndirectedGraphNode *));
+    
+    return dfs(graph, has, save);
+}
