963e40a97bc7f1dac3f35b96934a276351640616
gh843260227 <122433441@qq.com>
2018-07-31 17:52:55 +0800

    hdu1102.c

--- hdu1102.c
+++ hdu1102.c
@@ -10,14 +10,20 @@ typedef struct{
  
 void createMGraph(MGraph *mg)
 {
-     int i,j;
+     int i,j,n;
      scanf("%d",&(mg->vexnum));
      for (i = 0;i < mg->vexnum;i++)
-         mg->vexs[i] = i;
+         mg->vexs[i] = i+1;
      for (i = 0;i < mg->vexnum;i++)
      {
          for (j = 0;j < mg->vexnum;j++)
-             scanf("%d",&(mg->arcs[i][j]));
+         {
+             scanf("%d",&n);
+             if (n == 0)
+                 mg->arcs[i][j] = INT_MAX;
+             else
+                 mg->arcs[i][j] = n;
+         }
      }
 }
 
@@ -25,28 +31,21 @@ typedef struct
 {
     int adjvex;
     int lowcost;
-}close;
+}AR;
   
 int PRIM(MGraph mg,int n)
 {
     int i,j,k,sum=0;
-    close closedge[size];
-    int Q,W;
+    AR closedge[size];
+    int Q;
     scanf("%d",&Q);
-    W = Q;
     while (Q--)                   
     {
         scanf("%d %d",&i,&j);
-        closedge[i].lowcost = 0;
-        closedge[j].lowcost = 0;
-    }
-    while (closedge[n].lowcost == 0)
-    {
-       n++;
-       if (n == mg.vexnum)
-           n = n - mg.vexnum;
+        mg.arcs[i-1][j-1] = mg.arcs[j-1][i-1] = 0;
     }
     
+    int visited[size] = {1};
     for(i = 0;i < mg.vexnum;i++)
     {
         if(i != n)
@@ -55,23 +54,24 @@ int PRIM(MGraph mg,int n)
             closedge[i].adjvex = n;
         }
     }
-    closedge[n].lowcost = 0;
-    for(i = 1;i < mg.vexnum-W;i++)
+    visited[n] = 0;
+    for(i = 1;i < mg.vexnum;i++)
     {
         int min = INT_MAX,index = n;
         for (k = 0;k < mg.vexnum;k++)
         {
-            if(closedge[k].lowcost < min && closedge[k].lowcost!= 0)
+            if(closedge[k].lowcost < min && closedge[k].lowcost!= 0 && closedge[k].lowcost!=INT_MAX)
             {
                 min = closedge[k].lowcost;
                 index = k;
             }
         }
         sum += closedge[index].lowcost;
-        closedge[index].lowcost = 0;
+        visited[index] = 0;
+        
         for (j = 0;j < mg.vexnum;j++)
         {
-            if (mg.arcs[index][j] != 0 && mg.arcs[index][j] < closedge[j].lowcost)
+            if (visited[j] && mg.arcs[index][j] < closedge[j].lowcost)
             {
                closedge[j].adjvex = mg.vexs[index];
                closedge[j].lowcost = mg.arcs[index][j];
@@ -86,5 +86,6 @@ int main()
     MGraph G;
     createMGraph(&G);
     printf("%d",PRIM(G,G.vexs[0]));
+    system("pause");
     return 0;
 }
