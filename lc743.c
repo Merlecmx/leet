0138efa490404b4927efd0469fa177492c40a1d1
gh843260227 <122433441@qq.com>
2018-08-02 17:11:31 +0800

    lc743.c

--- lc743.c
+++ lc743.c
@@ -5,8 +5,8 @@ int networkDelayTime(int** times, int timesRowSize, int timesColSize, int N, int
     for (i = 1;i <= N;i++)
         for (j = 1;j <= N;j++)
             arcs[i][j] = INT_MAX;
-    for (i = 1;i < timesRowSize;i++)
-        arcs[times[i][1]][times[i][2]] = times[i][3];
+    for (i = 0;i < timesRowSize;i++)
+        arcs[times[i][0]][times[i][1]] = times[i][2];
     
     int *minlength = (int*)malloc(sizeof(int)*(N+1));       //V->Vi 最短路径
     int *visited = (int*)malloc(sizeof(int)*(N+1));         //访问过的点
@@ -16,11 +16,10 @@ int networkDelayTime(int** times, int timesRowSize, int timesColSize, int N, int
     }
     minlength[K] = 0;
     visited[K] = 1;
-    //int count = 1;
     for (i = 1;i < N;i++)         //剩下N-1 个点
     {
         int min = INT_MAX;
-        int v;
+        int v = K;
         for (j = 1;j <= N;j++)
         {
             if (!visited[j] && minlength[j] < min){
@@ -29,10 +28,9 @@ int networkDelayTime(int** times, int timesRowSize, int timesColSize, int N, int
             }    
         }
         visited[v] = 1;
-        //count++;
         for (j = 1;j <= N;j++)
         {
-            if (!visited[j] && (min+arcs[v][j]) < minlength[j])
+            if (!visited[j] && arcs[v][j]!= INT_MAX && (min+arcs[v][j]) < minlength[j])
                 minlength[j] = min+arcs[v][j];
         }
     }
@@ -42,15 +40,12 @@ int networkDelayTime(int** times, int timesRowSize, int timesColSize, int N, int
     {
         if (tol < minlength[i])
             tol = minlength[i];
-        if (minlength[i] == INT_MAX){
+        if (minlength[i] == INT_MAX)
             flag = 1;
-            break;
-        }
     }
-    //free(arcs);
+    free(arcs);
     free(minlength);
     free(visited);
     
-    //return count==N? tol : -1;
-    return flag==1? tol : -1;
+    return flag==0? tol : -1;
 }
