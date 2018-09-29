4c73e5cae7448e44c2908505d2917d20a19c9011
gh843260227 <122433441@qq.com>
2018-09-29 19:48:42 +0800

    Sodoku

--- lc37_dfs.c
+++ lc37_dfs.c
@@ -1,19 +1,15 @@
 bool place(char** board,int count,char ch)
 {
-    /*int i = count / 9;
-    int j = count % 9;
-    for (int k = 0;k < 9;k++)
-    {
-        int x = i / 3 * 3 + k / 3;
-        int y = j / 3 * 3 + k % 3;
-        if (board[i][k] == ch && i != k || board[k][j] == ch && j != k || board[x][y] == ch && x != i && y != j)
-            return false;
-    }*/
     int row = count / 9;
     int col = count % 9;
     for (int k = 0;k < 9;k++)
     {
-        if (ch == board[row][k] && k != col|| ch == board[k][col] && k != row)
+        if (ch == board[row][k] && k != col)
+            return false;
+    }
+    for (int t = 0;t < 9;t++)
+    {
+        if (ch == board[t][col] && t != row)
             return false;
     }
     for (int i = row/3 * 3;i < row/3 * 3 + 3;i++)
@@ -26,12 +22,16 @@ bool place(char** board,int count,char ch)
     }
     return true;
 }
+int flag = 0;
 void dfs_Sodoku(char** board,int count)
 {
-    if (count == 80)
-        return ;
     int i = count / 9;
     int j = count % 9;
+    if (count == 81){
+        //printf("Hello World!");
+        flag = 1;
+        return ;
+    }
     if (board[i][j] == '.')
     {
         for (char ch = '1';ch <= '9';ch++)
@@ -40,19 +40,17 @@ void dfs_Sodoku(char** board,int count)
             if (place(board,count,ch)){
                 dfs_Sodoku(board,count+1);
             }
-            board[i][j] == '.';
+            if (flag) return ;
+            board[i][j] = '.';
         }
     }
     else
-    {
         dfs_Sodoku(board,count+1);
-    }
 }
 void solveSudoku(char** board, int boardRowSize, int boardColSize)
 {
     //int row = boardRowSize,col = boardColSize;
     int count = 0;
-
-    dfs_Sodoku(board,count);
-    
+    flag = 0;
+    dfs_Sodoku(board,count);   
 }
