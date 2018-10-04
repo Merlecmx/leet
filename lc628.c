9bb08d16087c0203ae6678e77985925f2e5f0794
gh843260227 <122433441@qq.com>
2018-10-04 16:36:48 +0800

    lc628

--- lc628.c
+++ lc628.c
@@ -1,34 +1,9 @@
 class Solution {
 public:
-    int Partition(vector<int>& ar,int low,int high)
-    {
-        int pivotkey = ar[low];
-        while (low < high)
-        {
-            while (low < high && pivotkey <= ar[high])  high--;
-            ar[low] = ar[high];
-            while (low < high && pivotkey >= ar[low])  low++;
-            ar[high] = ar[low];
-        }
-        ar[low] = pivotkey;
-        return low;
-    }
-    void Qsort(vector<int>& nums,int low,int high)
-    {
-        if (low < high)
-        {
-            int k = Partition(nums,low,high);
-            Qsort(nums,low,k-1);
-            Qsort(nums,k+1,high);
-        }
-    }
     int maximumProduct(vector<int>& nums)
     {
+    	sort(nums.begin(),nums.end());
         int t = nums.size()-1;
-        Qsort(nums,0,t);
-        //for (int i = 0;i < nums.size();i++)
-            //cout << nums[i] << " ";
-        //cout << "\n" << endl;
         int target = 0;
         if (nums[t] > 0)
         {
