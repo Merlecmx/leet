abc2a1ea1a106be895314caf88c4898299c7e94c
gh843260227 <122433441@qq.com>
2018-10-22 21:18:31 +0800

    Tr A

---
+++ hdu1575.cpp
@@ -0,0 +1,68 @@
+#include<iostream>
+using namespace std;
+#define SIZE 11
+#define MOD 9973
+int n;
+class item {
+public:
+	int ar[SIZE][SIZE];
+	friend item operator *(const item &lhs, const item &rhs);
+};
+item operator *(const item &lhs, const item &rhs) {
+	item temp;
+	int i,j,k;
+	for (i = 0; i < n;i++)
+	{
+		for (j = 0;j < n;j++)
+		{
+			temp.ar[i][j] = 0;
+			for (k = 0;k < n;k++)
+			{
+				temp.ar[i][j] += lhs.ar[i][k] * rhs.ar[k][j];
+			}
+			temp.ar[i][j] %= MOD;
+		}
+	}
+	return temp;
+}
+item ans,res;
+void pow(int k)
+{
+	int i,j;
+	for (i = 0;i < n;i++)
+	{
+		for (j = 0;j < n;j++){
+			ans.ar[i][j] = (i == j);
+		}
+	}
+	while (k)
+	{
+		if (k & 1)
+			ans = ans * res;
+		res = res * res ;
+		k >>= 1;
+	}
+}
+int main()
+{
+	int T;
+	cin >> T;
+	while (T--)
+	{
+		int i,j,k;
+		cin >> n >> k;
+		for (i = 0;i < n;i++)
+		{
+			for (j = 0;j < n;j++)
+			{
+				cin >> res.ar[i][j];
+			}
+		}
+		pow(k);
+		int sum = 0;
+		for (i = 0;i < n;i++)
+			sum += ans.ar[i][i];
+		printf("%d\n",sum % 9973);
+	}
+	return 0;
+}
