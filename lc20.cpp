543b45c24daedbcb67002898dbca7a688ab86a47
gh843260227 <122433441@qq.com>
2018-11-08 19:02:53 +0800

    lc20

---
+++ lc20.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isValid(string s)
+    {
+        if (s.size() == 0) return true;
+        char* ar = new char[5000];
+        int i = 0,j;
+        ar[i++] = s[0];
+        for (j = 1;j < s.size();j++)
+        {
+            if (s[j] == ')'&&ar[i-1] == '(' || s[j] == '}'&&ar[i-1] == '{' || s[j] == ']'&& ar[i-1] == '[')
+                i--;
+            else
+                ar[i++] = s[j];
+        }
+        delete []ar;
+        return i == 0;
+    } 
+};
