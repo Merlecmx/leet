d5ca38c9489fb9d8b68014de492122b2689b7227
gh843260227 <122433441@qq.com>
2018-11-17 20:43:42 +0800

    hdu4323

--- hdu4323.cpp
+++ hdu4323.cpp
@@ -2,55 +2,35 @@
 #include<string>
 #include<vector>
 #include<algorithm>
-#include<cstdlib>
 using namespace std;
-int min_s(int a,int b,int c){
-    if (a < b){
-        if (c < a)
-            return c;
-        else
-            return a;
-    }
-    else{
-        if (c < b)
-            return c;
-        else
-            return b;
-    }
-}
-int minDistance(string word1, string word2)
+
+int minDistance(string &word1, string &word2)
 {
-    int len1 = word1.size();
-    int len2 = word2.size();
-    //int (*ar)[len2+1] = (int(*)[len2+1])malloc(sizeof(int)* (len1+1) * (len2+1));
     int ar[100][100];
     int i,j;
-    for (i = 0;i <= len1;i++) {
+    for (i = 0;i <= word1.size();i++) {
         ar[i][0] = i;
     }
-    for (j = 0;j <= len2;j++) {
+    for (j = 0;j <= word2.size();j++) {
         ar[0][j] = j;
     }
-    for (i = 1;i <= len1;i++)
+    for (i = 1;i <= word1.size();i++)
     {
-        for (j = 1;j <= len2;j++)
+        for (j = 1;j <= word2.size();j++)
         {
             if(word1[i-1] == word2[j-1])
                 ar[i][j] = ar[i-1][j-1];
             else
             {
-                ar[i][j] = min_s(ar[i-1][j], ar[i][j-1], ar[i-1][j-1]) + 1;
+                ar[i][j] = min(min(ar[i-1][j], ar[i][j-1]), ar[i-1][j-1]) + 1;
             }
         }
     }
-    int t = ar[len1][len2];
-    //free(ar);
-    return t;
+    return ar[word1.size()][word2.size()];
 }
 int main()
 {
     int id,T;
-    int sum[1002];
     cin >> T;
     for (id = 1;id <= T;id++)
     {
@@ -67,20 +47,15 @@ int main()
         int k,count;
         for (i = 0;i < m;i++)
         {
+            cout << "Case #" << id << ":" << endl;
             cin >> s >> k;
             count = 0;
             for (j = 0;j < n;j++)
             {
                 int t = minDistance(res[j],s);
-                if (t <= k)
-                    count++;
+                count += t <= k;
             }
-            sum[i] = count;
-        }
-        
-        cout << "Case #" << id << ":" << endl;
-        for (i = 0;i < m;i++) {
-            cout << sum[i] << endl;
+            cout << count << endl;
         }
     }
     return 0;
