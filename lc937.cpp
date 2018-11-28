7faf8a09b43945279ccf7d1d3334911cfc244805
gh843260227 <122433441@qq.com>
2018-11-28 21:05:47 +0800

    lc937

---
+++ lc937.cpp
@@ -0,0 +1,33 @@
+bool cmp(const pair<string, string>& a, const pair<string, string>& b) {
+    return a.second < b.second;
+}
+class Solution {
+public:
+    vector<string> reorderLogFiles(vector<string>& logs) {
+        int i,j;
+        vector<pair<string,string>> res;
+        vector<string> number;
+        for (auto log : logs)
+        {
+            if (log.back() >= 'a' && log.back() <= 'z') {
+                for (i = 0;log[i] != ' ';i++) ;
+                res.push_back(make_pair(log,log.substr(i+1)));
+            }
+            else {
+                number.push_back(log);
+            }
+        }
+        // for (i = 0;i < res.size();i++) {
+        //     cout << res[i].first << " " << res[i].second << endl << endl;
+        // }
+        sort(res.begin(),res.end(),cmp);
+        vector<string> result;
+        for (i = 0;i < res.size();i++) {
+            result.push_back(res[i].first);
+        }
+        for (i = 0;i < number.size();i++) {
+            result.push_back(number[i]);
+        }
+        return result;
+    }
+};
