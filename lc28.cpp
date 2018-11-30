84cb0544a343b2cdbd51a4640078e10a1582d0d9
gh843260227 <122433441@qq.com>
2018-11-30 18:57:35 +0800

    lc28

---
+++ lc28.cpp
@@ -0,0 +1,7 @@
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        string::size_type pos = haystack.find(needle);
+        return pos == string::npos ? -1 : pos;
+    }
+};
