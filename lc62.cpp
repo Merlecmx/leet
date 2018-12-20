1aea0f33b92a3681733d2c6e8c29d9ff3573692f
gh843260227 <122433441@qq.com>
2018-12-20 15:19:20 +0800

    lc62_dp.cpp

--- lc62.cpp
+++ lc62.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int uniquePaths(int m, int n) {
         int i,j;
-        int dp[m+1][n+1];   // dp[i][j]表示i行 j列的不同路径
+        int dp[m+1][n+1];
         for (i = 1,j = 1;j < n + 1;j++) {
             dp[i][j] = 1;
         }
