c1c199630ae39634691ed6454314f3117c0028d3
gh843260227 <122433441@qq.com>
2018-12-21 11:16:11 +0800

    lc357.cpp

--- lc357.cpp
+++ lc357.cpp
@@ -38,3 +38,33 @@ public:
         return true;
     }
 };
+
+class Solution {
+public:
+    int countNumbersWithUniqueDigits(int n) {
+        if (!n)
+            return 1;
+        int dp[11];
+        dp[0] = 10;
+        dp[1] = 81;
+
+        int i,count = 0;
+        for (i = 2;i <= 10;i++)
+        {
+            dp[i] = dp[i-1] * (10 - i);
+        }
+        if (n <= 10) {
+            for (i = 0;i < n;i++)
+            {
+                count += dp[i];
+            }
+        }
+        else {
+            for (i = 0;i <= 10;i++)
+            {
+                count += dp[i];
+            }
+        }
+        return count;
+    }
+};
